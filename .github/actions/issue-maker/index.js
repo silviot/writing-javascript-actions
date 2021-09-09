5faa64a103b512df19704831710e8afa4156d420
Silvio Tomatis <silviot@gmail.com>
2021-09-10 00:47:52 +0200

    Fix octokit invocation

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -10,7 +10,7 @@ async function run() {
 
     const octokit = new github.getOctokit(token);
 
-    await octokit.issues.create({
+    await octokit.rest.issues.create({
       repo: github.context.repo.repo,
       owner: github.context.repo.owner,
       title: issueTitle,
